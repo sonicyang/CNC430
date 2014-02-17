10a63fb5d47a73b76d80eda74e5c525101467ac4
Sonicyang <sonicyang@mollusca.cc>
2014-02-17 11:35:26 +0800

    Done

--- python/CNCctl.py
+++ python/CNCctl.py
@@ -1,11 +1,17 @@
 import serial
 
 
+ser = None
+flag = False
+
+
 def displayhelp():
     print("List of commands:")
     print("help -- display help")
     print("exit -- quit controller")
     print("shell -- interactive G-code shell")
+    print("start -- open serial port to communicate")
+    print("file -- start send code from file")
 
 
 def shell():
@@ -14,8 +20,20 @@ def shell():
     while True:
         code = input("G>")
         if(code == "return"):
-            print("returning to normal shell");
+            print("returning to normal shell")
             return
+        else:
+            ser.write((code.strip() + "\n").encode("ASCII"))
+            ser.read(1)
+
+
+def send():
+    fn = input("Please input G-Code file's filename:")
+    f = open(fn)
+    for l in f:
+        print(l.strip())
+        ser.write((l.strip() + "\n").encode("ASCII"))
+        ser.read(1)
 
 print("Welcome to CNC430 Contorl Program!")
 print("type in commands to operate; help to list commands")
@@ -29,4 +47,18 @@ while True:
     elif(comm == "help"):
         displayhelp()
     elif(comm == "shell"):
-        shell()
+        if(flag):
+            shell()
+        else:
+            print("Please open a serial port first")
+    elif(comm == "start"):
+        portnum = input("Input Serial port number:")
+        ser = serial.Serial(portnum, 9600, timeout=None)
+        flag = True
+    elif(comm == "file"):
+        if(flag):
+            send()
+        else:
+            print("Please open a serial port first")
+    else:
+        print("I can't get you ;(")
